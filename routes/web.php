4b0db21fcf7a24d9b5100c731fdd7a009b7937ab
Agasi Gilang Persada <464516p@gmail.com>
2025-01-08 17:26:35 +0700

    Bug fix route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::get('articles', ArticleListController::class)->name('articles.list');
+Route::get('articles/{article:slug}', [ArticleController::class, 'show'])->name('articles.show');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -18,7 +22,5 @@ Route::middleware('auth')->group(function () {
 
     Route::resource('articles', ArticleController::class);
 });
-Route::get('articles', ArticleListController::class)->name('articles.list');
-Route::get('articles/{article:slug}', [ArticleController::class, 'show'])->name('articles.show');
 
 require __DIR__.'/auth.php';
